d6c708935af508738e83d8f629f9eae95545cb91
Roman Garanin <evilkhaoskat@gmail.com>
2019-04-28 14:51:08 +0300

    add project skeleton

---
+++ backend/java/src/main/java/dev/romangaranin/spf/SimplePeakFlowMeterApplication.java
@@ -0,0 +1,13 @@
+package dev.romangaranin.spf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePeakFlowMeterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplePeakFlowMeterApplication.class, args);
+    }
+
+}
